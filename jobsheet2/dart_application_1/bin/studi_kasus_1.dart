aebfe62dbb33b6c961dd215fb8221c7245039044
hamdirafi <hamdirafi310@gmail.com>
2026-02-25 14:59:32 +0700

    jobsheet2

---
+++ jobsheet2/dart_application_1/bin/studi_kasus_1.dart
@@ -0,0 +1,5 @@
+void main() {  
+double celsius = 30;  
+double fahrenheit, kelvin;    
+fahrenheit = (celsius * 9/5) + 32;  
+kelvin = celsius + 273.15;    print('$celsius°C = $fahrenheit°F');  print('$celsius°C = $kelvin K');}
---
+++ tugas_praktikum/bin/tugas_praktikum.dart
@@ -0,0 +1,5 @@
+import 'package:tugas_praktikum/tugas_praktikum.dart' as tugas_praktikum;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tugas_praktikum.calculate()}!');
+}
